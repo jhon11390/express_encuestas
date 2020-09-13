d425217e8c7af22600bd7dcfa6fd0c9791d651cc
jhon11390 <meljhon2@gmail.com>
2020-09-13 04:01:16 -0500

    Agrega ruta para eliminar encuesta

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const Poll = require("./models/Poll")
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/polls", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use('/assets', express.static('assets'));
+app.use(methodOverride('_method'))
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const polls = await Poll.find();
+  res.render('index', {polls})
 });
 
-app.get('/new', (req, res) => {
+app.get('/polls/new', (req, res) => {
   res.render('new')
 })
 
-app.get('/prueba', (req, res) => {
-  res.render('poll')
-});
+app.post('/polls', async (req, res, next) => {
+  const data = {
+    title: req.body.title,
+    description: req.body.description,
+    option1: req.body.option1,
+    option2: req.body.option2,
+    option3: req.body.option3
+  };
+  try{
+    const poll = new Poll(data);
+    await poll.save()
+  }catch(e){
+    return next(e)
+  }
+  res.redirect("/")
+})
+
+app.delete("/polls/:id", async (req, res, next) => {
+  try{
+    await Poll.deleteOne({_id: req.params.id});
+  }catch(e){
+    return next(e)
+  }
+  res.redirect("/")
+})
 app.listen(3000, () => console.log('listeng on port 3000'));
