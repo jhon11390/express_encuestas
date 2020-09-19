a0e1d201a68147f6b32fc3b268f5fe758039427f
jhon11390 <meljhon2@gmail.com>
2020-09-19 02:34:58 -0500

    Arregla la funcion de mostrar la tercera opcion de encuestas

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,18 +1,18 @@
 
 $(document).ready(function(){
+	$('#addoption3').hide()
 	$('#add_field').click (function(e) {
-		e.preventDefault();     //prevenir novos clicks
-			$('#addoption3').append('<div class="input-group">\
-				<input type="text" name="option3" class="form-control" id="option3" placeholder="Opcion 3">\
-				<div class="input-group-append"><a href="#" class="remover_campo btn btn-primary"><i class="fas fa-trash-alt"></i></a></div>\
-				</div>');
+		e.preventDefault();
+		$('#addoption3').show();
+		$("#option3").attr("required", true);
 		$(this).hide();
 	});
 
-	$('#addoption3').on("click",".remover_campo",function(e) {
+	$('#button-addon2').click(function(e){
 		e.preventDefault();
-		$(this).parent('div').parent('div').remove();
-		$("#add_field").show();
-	});
+		$('#addoption3').hide();
+		$("#option3").attr("required", false);
+		$('#add_field').show();
+	})
 
 })
