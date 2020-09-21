7fd8e2f06fcb83d2adafdff75482549a86bd18c6
jhon11390 <meljhon2@gmail.com>
2020-09-21 10:10:17 -0500

    Agrega ventanas para compartir encuestas

--- assets/javascript.js
+++ assets/javascript.js
@@ -16,14 +16,6 @@ $(document).ready(function(){
 	})
 	$('#share-link').on('click', function(){
 		$('#myModal').modal('toggle');
-		console.log('show link');
 	});
-	  
-	$('#share-facebook').on('click', function(){
-		const link = $('#share-facebook').data('link')
-		FB.ui({
-		  method: 'share',
-		  href: link,
-		}, function(response){});
-	})
 })
+
